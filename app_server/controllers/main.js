a5aeeda01a000cab77845755e93f59cf0eb9ee6b
krish2898ad <kirankrishna2k4@gmail.com>
2024-07-14 16:25:04 +0530

    Update main.js

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,6 @@
 /* GET homepage */
 const index = (req, res) => {
-    res.render('index', { title: 'Tharun Anna' });
+    res.render('index', { title: 'Expresss' });
     };
     module.exports = {
         index
